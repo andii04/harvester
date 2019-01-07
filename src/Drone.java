c6b59ed7fde59f924f0d5c2358d1fc88122b8387
Schurk, Fabian <fabian.schurk@web.de>
2019-01-07 16:10:51 +0100

    saf

--- src/Drone.java
+++ src/Drone.java
@@ -2,12 +2,12 @@ import java.util.HashMap;
 
 public class Drone implements IDrone{
 
-    HashMap<String, Integer> hmap = new HashMap<>();
+    HashMap<Position, Integer> hmap = new HashMap<>();
     public void scan(WheatField wheatField)
     {
         for(int i =0; i<10000;i++){
             for(int j =0; j<10000;j++){
-                hmap.put(i +", "+j,(int) wheatField.field[i][j].numberOfGrain);
+                hmap.put(new Position(i,j),(int) wheatField.field[i][j].numberOfGrain);
 
             }
             }
--- src/Position.java
+++ src/Position.java
@@ -1,4 +1,8 @@
 public class Position {
+    public  Position(int x, int y){
+        this.x = x;
+        this.y =y;
+    }
     int x;
     int y;
 }
