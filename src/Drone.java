91223a7abcf27d49540da77c6defcf7c28e1ac46
Wörrlein, Andreas <Andreas.Woerrlein@tecalliance.net>
2019-01-07 15:32:01 +0100

    scan drone

--- src/Drone.java
+++ src/Drone.java
@@ -1,3 +1,9 @@
-public class Drone {
+public class Drone implements IDrone{
+
+    public void scan()
+    {
+        System.out.println("Scan erfolgreich");
+    }
+
 }
 
---
+++ src/IDrone.java
@@ -0,0 +1,4 @@
+public interface IDrone {
+    void scan();
+
+}
