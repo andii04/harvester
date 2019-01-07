106d56d90956b1294b141aff478e6c7a48474f8c
Schurk, Fabian <fabian.schurk@web.de>
2019-01-07 15:27:05 +0100

    saf

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,9 @@ public class Main {
     public static void main(String[] args) {
         WheatField wheatField = new WheatField();
         System.out.println(wheatField.field[0][0].numberOfGrain);
+        System.out.println(wheatField.field[0][1].numberOfGrain);
+        System.out.println(wheatField.field[0][2].numberOfGrain);
+        System.out.println(wheatField.field[0][3].numberOfGrain);
+        System.out.println(wheatField.field[0][4].numberOfGrain);
     }
 }
--- src/Wheat.java
+++ src/Wheat.java
@@ -2,7 +2,6 @@ public class Wheat {
 
     short numberOfGrain;
 
-
     public Wheat() {
         this.numberOfGrain = (short) (100 + (int)(Math.random() * ((500 - 100) + 1)));
     }
