6b1beca028894eaf634d86d4b595f48c78b12df9
Schurk, Fabian <fabian.schurk@web.de>
2019-01-07 14:42:53 +0100

    saf

--- src/Wheat.java
+++ src/Wheat.java
@@ -3,7 +3,7 @@ public class Wheat {
     int numberOfGrain;
 
 
-    public Wheat(int numberOfGrain) {
-        this.numberOfGrain = numberOfGrain;
+    public Wheat() {
+        numberOfGrain = 100 + (int)(Math.random() * ((500 - 100) + 1));
     }
 }
