76834bccee2e15b08ea760a8719e80aced0aee26
Wörrlein, Andreas <Andreas.Woerrlein@tecalliance.net>
2019-01-08 13:14:31 +0100

    better klammer

--- src/Drone.java
+++ src/Drone.java
@@ -5,13 +5,15 @@ public class Drone implements IDrone{
     HashMap<Position, Integer> hmap = new HashMap<>();
     public void scan(WheatField wheatField)
     {
-        for(int i =0; i<10000;i++){
-            for(int j =0; j<10000;j++){
+        for(int i =0; i<10000;i++)
+        {
+            for(int j =0; j<10000;j++)
+            {
                 hmap.put(new Position(i,j),(int) wheatField.field[i][j].numberOfGrain);
 
             }
-            }
+        }
     }
-    }
+}
 
 
