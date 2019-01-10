fae6b65310ff8d2728e27b5ebb404663a720e803
Wörrlein, Andreas <Andreas.Woerrlein@tecalliance.net>
2019-01-10 15:32:13 +0100

    finished

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 
     public static void main(String[] args) {
-        WheatField wheatField = new WheatField(2500); //Generate Field
+        WheatField wheatField = new WheatField(3000); //Generate Field
         //Generate Harvester
         Harvester harvester = new Harvester(wheatField, "Claas", 500, 200000, 30000);
         // DO the action with drone
